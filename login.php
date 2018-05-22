02cadc034d1bc45b8c11a30e6f744fa3efc1af14
ashraysinha6069 <iec2017048@iiita.ac.in>
2018-05-23 00:49:58 +0530

    initial view of login form which directs towards google

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+    include ("header.php");
+?>
+
+<div class="container content">
+    <hr/>
+    <div class="row mid-header">
+        <div class="col-md-12 col-xm-12 middle">
+            SIGN UP WITH YOUR IIITA ACCOUNT
+        </div>
+        <div class="col-md-12 col-xm-6 middle">
+            <img src="https://developers.google.com/+/images/branding/sign-in-buttons/Red-signin_Google_base_44dp.png" style="height:40%; border:0.5px solid green;">            
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+    include ("footer.php");
+?>
