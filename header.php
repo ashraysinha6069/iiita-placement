c92c43a61cd052b8eb46dc21bc0e18c40f26e18a
ashraysinha6069 <iec2017048@iiita.ac.in>
2018-05-28 19:28:06 +0530

    idth ncreased modal

--- header.php
+++ header.php
@@ -11,13 +11,16 @@
         <script src="assets/js/site.js"></script>
         <link href="css/main.css" rel="stylesheet" type="text/css"> 
         <link href="https://fonts.googleapis.com/css?family=Catamaran:900|Playfair+Display|Montserrat|Montserrat+Alternates|Fjalla+One" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     </head>
     <body>
         <br>
-            <div class="container content">
-                <div class="row">
-                    <div class="col-xs-12 col-md-12 heading" >DEPARTMENT OF TRAINING & PLACEMENT</div>
-                </div>
+        <div class="content">
+            <div class="row mid-header">
+                <div class="col-xs-12 col-md-12 heading" >DEPARTMENT OF TRAINING & PLACEMENT</div>
+            </div>
+        </div>
+            <div class="content">
                 <div class="row mid-header">
                     <div class="col-md-3 col-xm-3" style="text-align: center;">
                         <img class="iiita-logo" src="assets/images/IIIT_logo_transparent.gif">
