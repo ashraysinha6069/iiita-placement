993d1d067ee8113d23f99ecd7585fba75f55d4c6
ashraysinha6069 <iec2017048@iiita.ac.in>
2018-06-03 12:42:06 +0530

    student blacklisting started

---
+++ add_to_blacklist.php
@@ -0,0 +1,14 @@
+<?php
+
+    if(isset($_POST['id'])){
+        $id = $_POST['id'];
+        $connection = mysqli_connect("localhost","root","","iiita-placement");
+        $select_query = "UPDATE students SET
+                            student_status='1' 
+                            WHERE student_id='$id';";
+        mysqli_query($connection,$select_query);
+        $data['detail']="success";
+        
+        echo json_encode($data);
+    }
+?>
