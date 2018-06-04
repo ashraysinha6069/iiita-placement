8046adab6e73ca686658a093093025e81fd04ef8
ashraysinha6069 <iec2017048@iiita.ac.in>
2018-06-04 21:17:51 +0530

    jobs now sent to different years by admin

--- admin_profile.php
+++ admin_profile.php
@@ -22,26 +22,131 @@
 <div class="container">
   <div class="row">
 
-      <div class="col-sm-8" > <!-- start of companies div -->
+      <div class="col-sm-8" > <!-- start of jobs div -->
           <div class="mdb-company thumbnail">
-            <div class="mdb-heading">Jobs</font></div>
+                <div class="middle">Jobs</div>
+                <div class="container"><hr/></div>  
                 <div class="row">
-                      <div class="col-lg-4">
-                          <div class="form-group-option" >
-                                <div class="mdb-show-label">Show</div>
-                                <select class="form-control mdb-select" id="filterselect">
-                                  <option selected value="Unapproved">New</option>
-                                  <option value="Active">Active</option>
-                                  <option value="Inactive">Inactive</option>
-                                  <option value="Unpublished">Unpublished</option>
-                                  <option value="Published" >Published</option>
-                                  <option value="ALL" >All</option>
-                                </select>
-                          </div>
-                      </div>
+                    <div class="col-lg-4">
+                        <div class="form-group-option" >
+                                    <div class="mid-header">Show</div>
+                                    <select class="form-control mdb-select" id="filterselect">
+                                        <option selected value="2">All</option>
+                                        <option value="1">Approved</option>
+                                        <option value="0">Unapproved</option>
+                                    </select>
+                        </div>
+                    </div>
+                </div><br/>
+                <div id="table_container">
+                    
+                            <?php
+                              
+                                $connetion = mysqli_connect("localhost","root","","iiita-placement");
+                                $select_query = "SELECT * FROM jobs;";
+                                $check = mysqli_query($connection , $select_query);
+                                echo '<table class="table table-striped">
+                                      <tr style="font-size:16px;">
+                                          <th>Job ID<i class="fas fa-sort"></i></th>
+                                          <th>Company<i class="fas fa-sort"></i></th>
+                                          <th>Job type<i class="fas fa-sort"></i></th>
+                                          <th>CTC<i class="fas fa-sort"></i></th>
+                                          <th>job location <i class="fas fa-sort"></i></th>
+                                          <th>job status <i class="fas fa-sort"></i></th>
+                                      </tr>';
+                                while($row = mysqli_fetch_assoc($check)){
+                                    
+                                    $job_id = $row['job_id'];
+                                    $job_company = $row['job_company'];
+                                    $job_type = $row['job_type'];
+                                    $job_ctc = $row['job_ctc'];
+                                    $job_location = $row['job_location'];
+                                    $job_details = $row['job_details'];
+                                    $job_duration = $row['job_duration'];
+                                    if($row['job_is_approved'] == '0'){
+                                        $job_is_approved = 'To be approved';
+                                    }
+                                    else{
+                                        $job_is_approved = 'Approved';
+                                    }
+                                    $job_status = $row['job_is_approved'];
+                                    echo '<tr id="open_approve" class="table_row" data-toggle="modal" data-target="#open_approve_modal'.$job_id.'">
+                                                <td>'.$job_id.'</td>
+                                                <td>'.$job_company.'</td>
+                                                <td>'.$job_type.'</td>
+                                                <td>'.$job_ctc.'</td>
+                                                <td>'.$job_location.'</td>
+                                                <td>'.$job_is_approved.'</td>
+                                            </tr>';
+                                    
+                                    echo '
+                <!-- ---------modal class statrs---------- --> 
+
+                                <div class="modal fade" id="open_approve_modal'.$job_id.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                 <div class="modal-dialog modal-dialog-centered" role="document">
+                                     <div class="modal-content dialog-box">
+                                         <div class="modal-body" style="text-align:center;">
+                                         <div class="container">
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">Company</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_company.'</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">Job type</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_type.'</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">Job Duration</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_duration.'</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">CTC</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_ctc.'</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">Job Location</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_location.'</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-5" id="col-5" style="text-align:center;">Job Details</div>
+                                                    <div class="col-2" id="col-2" style="text-align:center;">:</div>
+                                                    <div class="col-5" id="col-5">'.$job_details.'</div>
+                                                </div>
+                                         </div>    
+                                         </div>';
+                                        if($job_status == '0'){
+                                            
+                                            echo '<div  class="year_checkbox"><hr/> 
+                                                     Select students to send job request<br/><br/>
+                                                          <input type="checkbox" name="years" value="btech_3rd"> B.Tech 3rd Year &nbsp &nbsp
+                                                          <input type="checkbox" name="years" value="btech_4th"> B.Tech 4th Year <br/>
+                                                          <input type="checkbox" name="years" value="dual_3rd"> Dual Degree 3rd Year &nbsp &nbsp
+                                                          <input type="checkbox" name="years" value="dual_4th"> Dual Degree 4th Year <br/>
+                                                          <input type="checkbox" name="years" value="dual_5th"> Dual Degree 5th Year <br/>
+                                                          <button type="submit" name="submit" value="'.$job_id.'" class="btn btn-success" onclick="approval('.$job_id.')">Approve</button>  
+                                                </div>';
+                                        };
+                                         
+                                 echo '</div>
+                                </div>
+                                </div>  
+            <!-- ------------modal class ends--------------- -->
+
+                                    ';
+                                    
+                                };
+                                echo '</table>';
+                                mysqli_close($connection);
+                              ?> 
                 </div>
             </div>
-      </div><!-- end of col-sm-8 means end of companies div -->
+          
+      </div><!-- end of jobs div -->
 
 
       <div class="col-sm-4" > <!-- start of blacklisting div -->
@@ -52,39 +157,10 @@
                     <input id="blacklisted_search" type="text" placeholder="search"/>
                     <ul id="blacklisted_list">
                         <?php
-                        while($data=mysqli_fetch_array($check_listed)){
-                            
-                            //student data
-                            echo '<li id="blacklisted_data">'.$data['student_roll_no'].'</li>';
-                            
-                            //modal template starts
-                            echo '
-                                <div class="modal fade" id="blacklisted_modal_'.$data['student_id'].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                 <div class="modal-dialog modal-dialog-centered" role="document">
-                                     <div class="modal-content dialog-box">
-                                         <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="company-name" style="font-size:20px; text-align:center;">
-                                            Are you sure you want to remove<br/>
-                                                '.$data['student_roll_no'].'    
-                                            </div><hr/>
-                                            <div style="text-align:center;">
-                                                <button id="accept_'.$data['student_id'].'" type="button" class="btn btn-success" data-dismiss="modal">Yes</button>
-                                                <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                </div>
-                            ';
-
-                            //modal template stops
-                            
-                        }
+                            while($data=mysqli_fetch_array($check_listed)){
+                                //student data
+                                echo '<li id="blacklisted_data">'.$data['student_roll_no'].'</li>';
+                            }
                         ?>
                     </ul>
                 </div>   
@@ -120,6 +196,43 @@
         });
     });
     
+    $(document).ready(function(){
+        $("#filterselect").on('change' , function(){
+           
+            var value = $(this).val();
+            $.ajax({
+               url: 'fetch_jobs.php',
+               type: 'POST',
+               data: 'request='+value,
+               beforeSend: function(){
+                   $("#table_container").html('working on it...');
+               },
+               success: function(data){
+                   $("#table_container").html(data);
+               },
+               
+            });
+        }); 
+    });
+    
+    function approval(id){
+        var year = [];
+        $.each($("input[name='years']:checked"), function(){
+           year.push($(this).val()); 
+        });
+        $.ajax({
+           url: 'approve_job.php',
+           type: 'POST',
+           dataType: 'JSON',
+           data:{id: id,
+                 year: year},
+           success: function(data){
+               alert(data.detail);
+               location.reload();
+           },
+        });
+    }
+
 </script>
 <?php
 
